de85ba694029b0fbb47bc3b80a9e44146680f1f2
EduardoKPVeiga <epontesveiga@gmail.com>
2025-11-22 14:20:35 -0300

    first commit

---
+++ Client/Client/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class ClientRaw
+{
+    static bool running = true;
+
+    static void Main(string[] args)
+    {
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+
+        try
+        {
+            clientSocket.Connect(remoteEP);
+            Console.WriteLine("Conectado ao servidor! Digite suas mensagens:");
+
+            Thread receiveThread = new Thread(() => ReceiveMessages(clientSocket));
+            receiveThread.Start();
+
+            while (running)
+            {
+                Console.Write("Eu: ");
+                string msg = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(msg)) continue;
+                if (msg == "exit") break;
+
+                byte[] msgBuffer = Encoding.UTF8.GetBytes(msg);
+
+                clientSocket.Send(msgBuffer);
+            }
+
+            running = false;
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Erro: " + e.Message);
+        }
+    }
+
+    static void ReceiveMessages(Socket s)
+    {
+        byte[] buffer = new byte[1024];
+        try
+        {
+            while (running)
+            {
+                int bytesRead = s.Receive(buffer);
+
+                if (bytesRead > 0)
+                {
+                    string msg = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("\rServer/Outro: " + msg);
+                    Console.Write("Eu: ");
+                }
+                else
+                {
+                    running = false;
+                    Console.WriteLine("\nDesconectado pelo servidor.");
+                }
+            }
+        }
+        catch
+        {
+            if (running) Console.WriteLine("\nErro na conexão.");
+            running = false;
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class ServerRaw
+{
+    static List<Socket> clients = new List<Socket>();
+    static object lockObj = new object();
+
+    static void Main(string[] args)
+    {
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 8080);
+
+        try
+        {
+            serverSocket.Bind(localEndPoint);
+            serverSocket.Listen(10);
+
+            Console.WriteLine("Servidor (Raw Socket) ouvindo na porta 8080...");
+
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+
+                lock (lockObj) clients.Add(clientSocket);
+                Console.WriteLine("Novo cliente conectado.");
+
+                Thread t = new Thread(() => HandleClient(clientSocket));
+                t.Start();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    static void HandleClient(Socket client)
+    {
+        byte[] buffer = new byte[1024];
+
+        try
+        {
+            while (true)
+            {
+                int bytesRead = client.Receive(buffer);
+
+                if (bytesRead == 0)
+                    break;
+
+                string content = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Recebido: " + content);
+
+                Broadcast(buffer, bytesRead, client);
+            }
+        }
+        catch (SocketException)
+        {
+        }
+        finally
+        {
+            lock (lockObj) clients.Remove(client);
+            if (client.Connected) client.Shutdown(SocketShutdown.Both);
+            client.Close();
+            Console.WriteLine("Cliente desconectado.");
+        }
+    }
+
+    static void Broadcast(byte[] data, int length, Socket sender)
+    {
+        lock (lockObj)
+        {
+            foreach (Socket c in clients)
+            {
+                if (c != sender && c.Connected)
+                {
+                    try
+                    {
+                        c.Send(data, length, SocketFlags.None);
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
